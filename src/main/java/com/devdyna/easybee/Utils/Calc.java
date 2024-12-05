2b2a6191f5180f300d077542bbc145705fa43361
DevDyna <andrearisorti2003@gmail.com>
2024-12-05 23:23:57 +0100

    Create Calc.java

---
+++ src/main/java/com/devdyna/easybee/Utils/Calc.java
@@ -0,0 +1,40 @@
+package com.devdyna.easybee.Utils;
+
+public class Calc {
+
+    public Calc(){
+
+    }
+
+    public static double rnd(double min, double max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    public static int rnd(int min, int max) {
+        return (int)Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    public static boolean rnd50() {
+        return Math.random() < 0.5;
+    }
+
+    public static boolean rnd75() {
+        return Math.random() < 0.75;
+    }
+
+    public static boolean rnd25() {
+        return Math.random() < 0.25;
+    }
+
+    public static boolean rnd99() {
+        return Math.random() < 0.99;
+    }
+
+    public static boolean rnd05() {
+        return Math.random() < 0.05;
+    }
+
+    public static boolean rnd10() {
+        return Math.random() < 0.1;
+    }
+}
